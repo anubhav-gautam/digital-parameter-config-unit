296545a763efa530075213701ed8f02202a75483
Anubhav Gautam <anubhavgautam9720169985@gmail.com>
2026-02-11 13:56:17 +0530

    CREATE DISPLAY OBJECT

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
 #define OLED_ADDR 0x3C
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1
 void setup() {
 // write your initialization code here
 }
